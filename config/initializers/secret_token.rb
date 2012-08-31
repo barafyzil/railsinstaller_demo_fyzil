63c90cdab605ed93ba2bcf8e40f1ae2e9b6a793d
Hafiz Atanda <atanda.hafiz@gmail.com>
2012-08-31 16:53:12 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fyzil::Application.config.secret_token = '2ee098ae2e3405b060a6f4005f9a43eb9aad681aa473f6d8add8033905da032c232c0389b396297f74e78dbfe112d3113137bd857fbfabf3f859521bb33b694e'
